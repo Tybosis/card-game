ae7482721b30ad3cc982666311917b1492462973
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-04 00:16:42 -0800

    define parameters of project with card and deck specs

---
+++ spec/deck_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'deck'
+
+describe Deck do
+  before(:each) do
+    @deck = Deck.new
+  end
+
+  it 'initializes with 52 cards' do
+    @deck.cards.size.must_equal 52
+  end
+
+  it 'responds to a shuffle method' do
+    @deck.must_respond_to :shuffle!
+  end
+
+  it 'responds to a draw method' do
+    @deck.must_respond_to :draw
+  end
+
+  it 'draw method returns top card from the deck' do
+    top_card = @deck.cards[0]
+    @deck.draw.must_equal [top_card]
+  end
+
+  it 'draw method can return more than one card' do
+    @deck.draw(5).size.must_equal 5
+  end
+
+  it 'responds to a count method' do
+    @deck.must_respond_to :count
+  end
+
+  it 'count returns cards left in deck' do
+    @deck.count.must_equal 52
+    @deck.draw
+    @deck.count.must_equal 51
+  end
+end
